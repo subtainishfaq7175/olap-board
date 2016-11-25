06bb803a3e548eb5c39e9498b1d6c62cfd7c4324
subtainishfaqstudent <subtain>
2016-11-25 16:18:32 +0500

    adds Drag and drop view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app works!';
+  listBoxers:Array<string> = ['Sugar Ray Robinson','Muhammad Ali','George Foreman','Joe Frazier','Jake LaMotta','Joe Louis','Jack Dempsey','Rocky Marciano','Mike Tyson','Oscar De La Hoya'];
+  listTeamOne:Array<string> = [];
+  listTeamTwo:Array<string> = [];
 }
