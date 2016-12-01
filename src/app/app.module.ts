7f933c7095dc611b7d9571a7038907c0fd9d9ba9
subtainishfaqstudent <subtain>
2016-12-01 23:59:53 +0500

    components changed according to the API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,19 @@ import { ColomComponent } from './colom/colom.component';
 import { ItemComponent } from './colom/item/item.component';
 import { RowComponent } from './row/row.component';
 import { ConstantsService } from './common/contants.service';
+import { StoryComponent } from './story/story.component';
+import { TaskComponent } from './story/task/task.component';
+import { CategoryComponent } from './story/category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ColomComponent,
     ItemComponent,
-    RowComponent
+    RowComponent,
+    StoryComponent,
+    TaskComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/story/story.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-story',
+  templateUrl: './story.component.html'
+})
+export class StoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
